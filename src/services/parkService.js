40691015966ac4f6f13d2feb2530a2d1edc9a8bf
keeword <kee.word@gmail.com>
2025-01-07 16:48:07 +0700

    feat : add consolelog BASE URL

--- src/services/parkService.js
+++ src/services/parkService.js
@@ -2,6 +2,7 @@ const BASE_URL= `${import.meta.env.VITE_PARK_SERVER_URL}`;
 const AIRTABLE_ACCESS_TOKEN = `${import.meta.env.VITE_AIRTABLE_ACCESS_TOKEN}`
 
 const index = async () => {
+    console.log(BASE_URL)
     try{
         const res = await fetch(BASE_URL,{
             headers: {
